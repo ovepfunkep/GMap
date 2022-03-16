e25e8269f61c9253f653b32752d0fbeafdd8d420
ovepfunkep <Ivan_Tyabin@mail.ru>
2022-03-16 13:48:22 +0700

    Add project files.

---
+++ ForRIT/MainForm.cs
@@ -0,0 +1,54 @@
+﻿using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ForRIT.Utilities;
+using GMap.NET.Projections;
+
+namespace ForRIT
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache; 
+            gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            MapMarker CenterMarker = Methods.GetMapMarkerFromDB("Center");
+            gMapControl1.Position = new GMap.NET.PointLatLng(CenterMarker.Latitude, CenterMarker.Longitude);
+            gMapControl1.DragButton = MouseButtons.Left; 
+            gMapControl1.ShowCenter = false;
+            gMapControl1.Overlays.Add(Methods.CreateOverlay("Markers"));
+            foreach (GMapMarker m in gMapControl1.Overlays.SelectMany(m => m.Markers))
+                gMapControl1.UpdateMarkerLocalPosition(m);
+        }
+
+        private GMapMarker selectedMarker = null;
+        private void gMapControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            selectedMarker = gMapControl1.Overlays
+                .SelectMany(o => o.Markers)
+                .FirstOrDefault(m => m.IsMouseOver == true);
+        }
+
+        private void gMapControl1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (selectedMarker == null) return;
+            var latlng = gMapControl1.FromLocalToLatLng(e.X, e.Y);
+            selectedMarker.Position = latlng;
+            Methods.UpdateMarker(selectedMarker);
+            selectedMarker = null;
+        }
+    }
+}
---
+++ ForRIT/Utilities.cs
@@ -0,0 +1,90 @@
+﻿using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ForRIT
+{
+    internal class Utilities
+    {
+        public static readonly string SQLString = @"Data Source=DESKTOP-M7EVVNL\SQLEXPRESS;Initial Catalog=MapMarkers;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public class MapMarker
+        {
+            public int Id { get; set; }
+            public string TName { get; set; }
+            public double Latitude { get; set; }
+            public double Longitude { get; set; }
+        }
+
+        public class Methods
+        {
+            public static MapMarker GetMapMarkerFromDB(string TName)
+            {
+                MapMarker mapMarker = new MapMarker();
+                using (SqlConnection connection = new SqlConnection(SQLString))
+                {
+                    string query = "Select * from Technique where TName = @TName";
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("TName", TName);
+                    SqlDataReader reader = command.ExecuteReader();
+                    reader.Read();
+                    mapMarker.Id = (int)reader["Id"];
+                    mapMarker.TName = TName;
+                    mapMarker.Latitude = (double)(decimal)reader["Latitude"];
+                    mapMarker.Longitude = (double)(decimal)reader["Longitude"];
+                }
+                return mapMarker;
+            }
+
+            public static void UpdateMarker (GMapMarker gMapMarker)
+            {
+                using (SqlConnection connection = new SqlConnection(SQLString))
+                {
+                    string query = "Update Technique set Latitude = @Latitude, Longitude = @Longitude where TName = @TName";
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("Latitude", gMapMarker.Position.Lat);
+                    command.Parameters.AddWithValue("Longitude", gMapMarker.Position.Lng);
+                    command.Parameters.AddWithValue("TName", gMapMarker.ToolTipText);
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            public static GMarkerGoogle AddMarker (MapMarker mapMarker, GMarkerGoogleType gMarkerGoogleType = GMarkerGoogleType.red_dot)
+            {
+                GMarkerGoogle gMarker = new GMarkerGoogle(new GMap.NET.PointLatLng(mapMarker.Latitude, mapMarker.Longitude), gMarkerGoogleType);
+                gMarker.ToolTip = new GMap.NET.WindowsForms.ToolTips.GMapRoundedToolTip(gMarker);
+                gMarker.ToolTipText = mapMarker.TName;
+                gMarker.ToolTipMode = MarkerTooltipMode.OnMouseOver;
+                return gMarker;
+            }
+
+            public static GMapOverlay CreateOverlay (string Name, GMarkerGoogleType gMarkerGoogleType = GMarkerGoogleType.red_dot)
+            {
+                GMapOverlay gMapMarkers = new GMapOverlay(Name);
+                using (SqlConnection connection = new SqlConnection(SQLString))
+                {
+                    string query = "Select * from Technique";
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (reader["TName"].ToString() != "Center") gMapMarkers.Markers.Add(
+                            AddMarker(
+                                GetMapMarkerFromDB(reader["TName"].ToString())));
+                    }
+                }
+                return gMapMarkers;
+            }
+
+        }
+    }
+}
